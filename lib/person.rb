ac10c702bfaeeaa0f05c81504dac0ec316a10bb7
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-08 20:28:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,17 +2,6 @@ class Person
  
   def initialize(name)
     @name = name
-    
   end
- 
-  # def name=(full_name)
-  #   first_name, last_name = full_name.split
-  #   @first_name = first_name
-  #   @last_name = last_name
-  # end
- 
-  # def name
-  #   "#{@first_name} #{@last_name}".strip
-  # end
  
 end
